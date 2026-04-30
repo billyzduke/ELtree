f869743373d968022bd3cbdbb0484c3529ea40b6
billyZduke <billyzduke@gmail.com>
2026-04-30 15:08:58 -0700

    further tweaks to tree design

--- eltree/sketch.js
+++ eltree/sketch.js
@@ -21,7 +21,7 @@ function setup() {
   strokeWeight(1.5);
   noFill();
   
-  randomSeed(42); 
+  randomSeed(16); // Updated seed
   
   for (let i = 0; i < 1000; i++) {
     lengthVariations.push(random(0.5, 1.5)); 
@@ -43,11 +43,9 @@ function draw() {
   for (let i = 0; i < allWires.length; i++) {
     let wire = allWires[i];
     beginShape();
-    
     let c = color(wire.color);
     c.setAlpha(180); 
     stroke(c); 
-    
     for (let p of wire.path) {
       vertex(p.x, p.y, p.z);
     }
@@ -56,8 +54,7 @@ function draw() {
 }
 
 function getBundleOffset(wireIndex) {
-  let ox = 0;
-  let oy = 0;
+  let ox = 0, oy = 0;
   let remainder = wireIndex;
   let groupSize = totalWires;
   let currentAngle = 0;
@@ -66,43 +63,36 @@ function getBundleOffset(wireIndex) {
     let numSplits = splits[d];
     let prevGroupSize = groupSize;
     groupSize = groupSize / numSplits; 
-    
     let localBranch = floor(remainder / groupSize);
     remainder = remainder % groupSize;
     
     let isSpine = (d === splits.length - 1 && localBranch === 6);
-    
     let angleOffset = 0;
+    
     if (numSplits === 7) {
       angleOffset = (TWO_PI * localBranch) / 6;
     } else if (numSplits === 5) {
       let junctionID = floor(wireIndex / prevGroupSize);
       let slot = (localBranch + junctionID) % 6;
       angleOffset = (TWO_PI * slot) / 6;
+    } else if (numSplits === 3) {
+      angleOffset = (TWO_PI * localBranch) / 3;
     } else {
       if (d === 1 && numSplits === 2) {
-        if (splayInward) {
-          angleOffset = (localBranch === 0) ? (2 * PI / 3) : (-2 * PI / 3);
-        } else {
-          angleOffset = (localBranch === 0) ? (PI / 3) : (-PI / 3);
-        }
+        angleOffset = splayInward ? (2 * PI / 3) : (PI / 3);
+        if (localBranch !== 0) angleOffset *= -1;
       } else {
         angleOffset = (TWO_PI * localBranch) / numSplits;
       }
     }
     
     let branchAngle = currentAngle + angleOffset;
-    
     if (!isSpine) {
       let radius = sqrt(groupSize) * wireSpacing * bundleTightness;
-      if (d === splits.length - 1) {
-         radius = wireSpacing; 
-      }
-      
+      if (d === splits.length - 1) radius = wireSpacing; 
       ox += cos(branchAngle) * radius;
       oy += sin(branchAngle) * radius;
     }
-    
     currentAngle = branchAngle; 
   }
   return createVector(ox, oy, 0);
@@ -120,11 +110,9 @@ function calculateCoilPath(wireIndex) {
   let groupSize = totalWires;
 
   points.push(p5.Vector.add(currentPos, physicalOffset));
-  
   let d0_groupSize = totalWires / splits[0];
   let d0_bundleID = floor(wireIndex / d0_groupSize);
-  let baseHash = (d0_bundleID * 7) % lengthVariations.length;
-  let baseMod = 1.0 + (lengthVariations[baseHash] - 1.0) * 0.8; 
+  let baseMod = 1.0 + (lengthVariations[(d0_bundleID * 7) % 1000] - 1.0) * 0.8; 
   
   currentPos.z += (baseLen * baseMod);
   points.push(p5.Vector.add(currentPos, physicalOffset));
@@ -133,19 +121,19 @@ function calculateCoilPath(wireIndex) {
     let numSplits = splits[d];
     let prevGroupSize = groupSize;
     groupSize = groupSize / numSplits; 
-    
     let localBranch = floor(remainder / groupSize);
     let globalBundleID = floor(wireIndex / groupSize); 
     remainder = remainder % groupSize;
     
-    let uniqueNodeID = d * 1000 + globalBundleID;
-    let lengthMod = lengthVariations[uniqueNodeID % lengthVariations.length];
+    let lengthMod = lengthVariations[(d * 1000 + globalBundleID) % 1000];
     let distFromCenter = dist(0, 0, currentPos.x, currentPos.y);
-    let roundnessMod = map(distFromCenter, 0, 250, 1.5, 0.4, true);
+    let roundnessMod = map(distFromCenter, 0, 200, 2.2, 0.2, true);
 
     let isFinalLevel = (d === splits.length - 1);
     let isSpine = (isFinalLevel && localBranch === 6);
-    let finalSquash = isFinalLevel ? 0.5 : 1.0; 
+    
+    // UPDATED: finalSquash reduced to 0.3 to make berry stems shorter
+    let finalSquash = isFinalLevel ? 0.3 : 1.0; 
 
     let angleOffset = 0;
     if (numSplits === 7) {
@@ -154,6 +142,8 @@ function calculateCoilPath(wireIndex) {
       let junctionID = floor(wireIndex / prevGroupSize);
       let slot = (localBranch + junctionID) % 6;
       angleOffset = (TWO_PI * slot) / 6;
+    } else if (numSplits === 3) {
+      angleOffset = (TWO_PI * localBranch) / 3;
     } else {
       if (d === 1 && numSplits === 2) {
         angleOffset = splayInward ? (2 * PI / 3) : (PI / 3);
@@ -178,7 +168,7 @@ function calculateCoilPath(wireIndex) {
         let nextNumSplits = splits[d + 1];
         let nextGroupSize = groupSize / nextNumSplits;
         let nextBundleID = floor(wireIndex / nextGroupSize);
-        let hash = (nextBundleID * 11 + d * 17) % lengthVariations.length;
+        let hash = (nextBundleID * 11 + d * 17) % 1000;
         peelStagger = 1.0 + (lengthVariations[hash] - 1.0) * 0.7; 
       }
 
@@ -189,29 +179,36 @@ function calculateCoilPath(wireIndex) {
       let elbowPos = p5.Vector.add(currentPos, createVector(dirX * elbowLen, dirY * elbowLen, 0));
       let fingerPos = p5.Vector.add(elbowPos, createVector(0, 0, fingerLen));
 
-      // Calculate Rotation Matrix for the Horizontal Segment
-      let sinA = sin(branchAngle);
-      let cosA = cos(branchAngle);
-      
-      // This creates a circular cross-section perpendicular to the branch direction
-      let rotX = physicalOffset.x * (sinA * sinA) - physicalOffset.y * (sinA * cosA);
-      let rotY = -physicalOffset.x * (sinA * cosA) + physicalOffset.y * (cosA * cosA);
-      let rotZ = -(physicalOffset.x * cosA + physicalOffset.y * sinA);
-      let rotatedOffset = createVector(rotX, rotY, rotZ);
+      let perpX = -sin(branchAngle), perpY = cos(branchAngle);
+      let lat = physicalOffset.x * perpX + physicalOffset.y * perpY; 
+      let lon = physicalOffset.x * cos(branchAngle) + physicalOffset.y * sin(branchAngle);
+      let rotatedOffset = createVector(lat * perpX, lat * perpY, lon);
 
-      // 1. Point at the vertical-to-horizontal transition (starts rotating)
+      points.push(p5.Vector.add(currentPos, physicalOffset));
       points.push(p5.Vector.add(currentPos, rotatedOffset)); 
-      // 2. Point at the end of the horizontal run (still rotated)
       points.push(p5.Vector.add(elbowPos, rotatedOffset));
-      // 3. Point at the horizontal-to-vertical transition (snaps back to vertical offset)
+      points.push(p5.Vector.add(elbowPos, physicalOffset));
       points.push(p5.Vector.add(fingerPos, physicalOffset));
 
       currentPos = fingerPos.copy();
       currentAngle = branchAngle; 
     }
-    
     currentHorizLen *= 0.7; 
     currentVertLen *= 1.14;  
   }
+
+  // BERRY SPIRAL
+  let berryRadius = 4.2; 
+  let segments = 24;   
+  let turns = 3;       
+  for (let i = 0; i <= segments; i++) {
+    let lat = map(i, 0, segments, -HALF_PI, HALF_PI); 
+    let lon = map(i, 0, segments, 0, TWO_PI * turns); 
+    let x = berryRadius * cos(lat) * cos(lon + wireIndex); 
+    let y = berryRadius * cos(lat) * sin(lon + wireIndex);
+    let z = berryRadius * sin(lat) + berryRadius; 
+    points.push(p5.Vector.add(currentPos, createVector(x, y, z)));
+  }
+
   return points;
 }
